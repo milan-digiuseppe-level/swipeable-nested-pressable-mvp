a85d4b00c7dba23b723c17ede517f41dd5ec424c
Milan DiGiuseppe <milan.digiuseppe@level.com>
2024-04-12 17:39:35 -0400

    mvp

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Pressable, SafeAreaView } from "react-native";
+import Swipeable from "react-native-gesture-handler/Swipeable";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
+export default function App() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaView style={styles.container}>
+        <Swipeable
+          renderRightActions={() => (
+            <View style={{ width: 100, backgroundColor: "red" }} />
+          )}
+        >
+          <Pressable
+            onPress={() => alert("pressed!")}
+            style={{
+              padding: 20,
+              backgroundColor: "pink",
+              alignItems: "center",
+            }}
+          >
+            <Text>Nested pressable</Text>
+          </Pressable>
+        </Swipeable>
+      </SafeAreaView>
+    </GestureHandlerRootView>
+  );
+}
